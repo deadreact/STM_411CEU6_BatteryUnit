decf5b5c3dad6a42fd829475ecfdb91bf5c98c73
deadreact <deadreact@gmail.com>
2024-04-03 14:40:24 +0300

    initial commit

---
+++ TouchGFX/App/app_touchgfx.c
@@ -0,0 +1,86 @@
+/**
+  ******************************************************************************
+  * File Name          : app_touchgfx.c
+  ******************************************************************************
+  * This file was created by TouchGFX Generator 4.23.2. This file is only
+  * generated once! Delete this file from your project and re-generate code
+  * using STM32CubeMX or change this file manually to update it.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "app_touchgfx.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "ili9341.h"
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void touchgfx_init(void);
+void touchgfx_components_init(void);
+void touchgfx_taskEntry(void);
+
+/* USER CODE BEGIN PFP */
+static uint8_t isTransmittingData = 0;
+
+uint32_t touchgfxDisplayDriverTransmitActive(void)
+{
+	return isTransmittingData;
+}
+
+void touchgfxDisplayDriverTransmitBlock(uint8_t* pixels, uint16_t x, uint16_t y, uint16_t w, uint16_t h)
+{
+	isTransmittingData = 1;
+	ILI9341_SetWindow(x, y, x+w-1, y+h-1);
+	ILI9341_DrawBitmap(w, h, pixels);
+}
+/* USER CODE END PFP */
+
+/**
+ * Initialize TouchGFX application
+ */
+void MX_TouchGFX_Init(void)
+{
+    // Calling forward to touchgfx_init in C++ domain
+    touchgfx_components_init();
+    touchgfx_init();
+}
+
+/**
+ * TouchGFX application entry function
+ */
+void MX_TouchGFX_Process(void)
+{
+    // Calling forward to touchgfx_taskEntry in C++ domain
+    touchgfx_taskEntry();
+}
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
