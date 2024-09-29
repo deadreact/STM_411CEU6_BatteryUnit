4d601e03232fe791c8dedeee9589935bede0bc40
deadreact <deadreact@gmail.com>
2024-09-29 15:00:35 +0300

    charger_dcOk -> pull-down

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -610,8 +610,8 @@ static void MX_GPIO_Init(void)
 
   /*Configure GPIO pin : charger_dcOk_Pin */
   GPIO_InitStruct.Pin = charger_dcOk_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(charger_dcOk_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : pot_CS2_Pin pot_UD_Pin pot_INC_Pin pot_CS1_Pin
