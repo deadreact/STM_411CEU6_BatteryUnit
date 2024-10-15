c313b05c2a598daa4c50e969cf2e6a4d2ee6a4e8
deadreact <deadreact@gmail.com>
2024-10-15 15:11:58 +0300

    changed brightness settings

--- Application/include/settings_data.h
+++ Application/include/settings_data.h
@@ -50,7 +50,7 @@ struct SettingsData
     {
     	settings.push_back({"Charge power", {250, 500, 750, 1000, 1200}, 4});
 		settings.push_back({"Screen type", {0, 1}, 1});
-		settings.push_back({"Brightness", {25, 50, 75, 100}, 3});
+		settings.push_back({"Brightness", {10, 30, 70, 100}, 3});
 		selectedIndex = 0;
     }
 
