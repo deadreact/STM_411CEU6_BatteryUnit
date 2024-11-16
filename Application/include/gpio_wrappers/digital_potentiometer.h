0fd1adc66f003c05be658cf859997a3a8a55990b
deadreact <deadreact@gmail.com>
2024-11-16 13:24:27 +0200

    removed unused UART1, reconfig digitalPot class, restored port.c

--- Application/include/gpio_wrappers/digital_potentiometer.h
+++ Application/include/gpio_wrappers/digital_potentiometer.h
@@ -15,8 +15,10 @@
 class DigitalPotentiometer
 {
 public:
-	DigitalPotentiometer(GPIO_TypeDef* chipSelect_GPIOx, uint16_t chipSelect_GPIO_Pin, bool inversedUD = false)
-		: m_potCS(chipSelect_GPIOx, chipSelect_GPIO_Pin)
+	DigitalPotentiometer(PinWrapper cs, PinWrapper inc, PinWrapper ud, bool inversedUD = false)
+		: m_potCS(cs)
+	    , m_potINC(inc)
+	    , m_potUD(ud)
 		, m_inversedUD(inversedUD)
 	{}
 
@@ -30,8 +32,8 @@ private:
 	void changeValue(bool increase);
 
 	PinWrapper m_potCS;
-//	PinWrapper m_potINC{pot_INC_GPIO_Port, pot_INC_Pin};
-//	PinWrapper m_potUD{pot_UD_GPIO_Port, pot_UD_Pin};
+	PinWrapper m_potINC;
+	PinWrapper m_potUD;
 
 	uint8_t m_currentValue{100};
 	uint8_t m_goalValue{0};
@@ -42,43 +44,45 @@ private:
 
 //----------------------------------------------------------------
 
-//class AsyncDigitalPotentiometer
-//{
-//public:
-//	enum class CSState
-//	{
-//		Selected,
-//		Deselection,
-//		Unselected
-//	};
-//
-//	AsyncDigitalPotentiometer(GPIO_TypeDef* chipSelect_GPIOx, uint16_t chipSelect_GPIO_Pin, bool inversedUD = false)
-//		: m_potCS(chipSelect_GPIOx, chipSelect_GPIO_Pin)
-//		, m_csState(m_potCS.readPin() ? CSState::Unselected : CSState::Selected)
-//		, m_inversedUD(inversedUD)
-//	{}
-//
-//	void onTick();
-//
-//	void setValue(uint8_t value);
-//	uint8_t getValue() const { return m_currentValue; }
-//private:
-//	bool select();
-//	void deselect(bool store = false);
-//	void changeValue(bool increase);
-//
-//	PinWrapper m_potCS;
-//	PinWrapper m_potINC{pot_INC_GPIO_Port, pot_INC_Pin};
-//	PinWrapper m_potUD{pot_UD_GPIO_Port, pot_UD_Pin};
-//
-//	uint8_t m_currentValue{100};
-//	uint8_t m_goalValue{0};
-//	CTimeout m_incTimeout{1};
-//	CTimeout m_deselectionTimeout{20};
-//
-//	CSState m_csState;
-//
-//	const bool m_inversedUD;
-//};
+class AsyncDigitalPotentiometer
+{
+public:
+	enum class CSState
+	{
+		Selected,
+		Deselection,
+		Unselected
+	};
+
+	AsyncDigitalPotentiometer(PinWrapper cs, PinWrapper inc, PinWrapper ud, bool inversedUD = false)
+		: m_potCS(cs)
+		, m_potINC(inc)
+		, m_potUD(ud)
+		, m_csState(m_potCS.readPin() ? CSState::Unselected : CSState::Selected)
+		, m_inversedUD(inversedUD)
+	{}
+
+	void onTick();
+
+	void setValue(uint8_t value);
+	uint8_t getValue() const { return m_currentValue; }
+private:
+	bool select();
+	void deselect(bool store = false);
+	void changeValue(bool increase);
+
+	PinWrapper m_potCS;
+	PinWrapper m_potINC;
+	PinWrapper m_potUD;
+
+	uint8_t m_currentValue{100};
+	uint8_t m_goalValue{0};
+	CTimeout m_incTimeout{1};
+	CTimeout m_deselectionTimeout{20};
+
+	CSState m_csState;
+
+	const bool m_inversedUD;
+};
 
 #endif /* INCLUDE_GPIO_WRAPPERS_DIGITAL_POTENTIOMETER_H_ */
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -54,7 +54,6 @@ DMA_HandleTypeDef hdma_spi1_tx;
 TIM_HandleTypeDef htim2;
 TIM_HandleTypeDef htim4;
 
-UART_HandleTypeDef huart1;
 UART_HandleTypeDef huart2;
 
 /* Definitions for defaultTask */
@@ -86,7 +85,6 @@ static void MX_USART2_UART_Init(void);
 static void MX_RTC_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_ADC1_Init(void);
-static void MX_USART1_UART_Init(void);
 void StartDefaultTask(void *argument);
 void StartGUITask(void *argument);
 
@@ -136,7 +134,6 @@ int main(void)
   MX_RTC_Init();
   MX_TIM2_Init();
   MX_ADC1_Init();
-  MX_USART1_UART_Init();
   MX_TouchGFX_Init();
   /* Call PreOsInit function */
   MX_TouchGFX_PreOSInit();
@@ -559,39 +556,6 @@ static void MX_TIM4_Init(void)
   /* USER CODE BEGIN TIM4_Init 2 */
 
   /* USER CODE END TIM4_Init 2 */
-
-}
-
-/**
-  * @brief USART1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_USART1_UART_Init(void)
-{
-
-  /* USER CODE BEGIN USART1_Init 0 */
-
-  /* USER CODE END USART1_Init 0 */
-
-  /* USER CODE BEGIN USART1_Init 1 */
-
-  /* USER CODE END USART1_Init 1 */
-  huart1.Instance = USART1;
-  huart1.Init.BaudRate = 115200;
-  huart1.Init.WordLength = UART_WORDLENGTH_8B;
-  huart1.Init.StopBits = UART_STOPBITS_1;
-  huart1.Init.Parity = UART_PARITY_NONE;
-  huart1.Init.Mode = UART_MODE_TX_RX;
-  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN USART1_Init 2 */
-
-  /* USER CODE END USART1_Init 2 */
 
 }
 
